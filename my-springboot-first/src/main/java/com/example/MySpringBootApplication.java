55afe209f2a128e3d1edf0173d7ae26b6354c0ac
Xusl <610031226@qq.com>
2020-06-20 20:15:41 +0800

    项目推送到本地仓库

---
+++ my-springboot-first/src/main/java/com/example/MySpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Description:
+ * @Title: MySpringBoot
+ * @PackageName: com.example
+ * @ProjectName: ecp
+ * @Author: Xusl
+ * @Date: 2020-6-18 22:46
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
